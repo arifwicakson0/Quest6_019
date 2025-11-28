aa85142ccf6dd019df26d17ed74f632ba1893c8f
arifwicakson0 <wicaksonoarif487@gmail.com>
2025-11-28 19:46:33 +0700

    menambah DatabaseSiswa

---
+++ 019_RoomDatabase/app/src/main/java/com/example/a019_roomdatabase/room/DatabaseSiswa.java.kt
@@ -0,0 +1,25 @@
+package com.example.a019_roomdatabase.room
+
+import android.content.Context
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+
+    abstract fun siswaDao(): SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return Instance ?: synchronized(lock = this){
+                Room.databaseBuilder(
+                    context, klass = DatabaseSiswa::class.java,
+                    name = "siswa_database")
+                    .build().also { Instance=it }
+            }
+        }
+    }
+}
